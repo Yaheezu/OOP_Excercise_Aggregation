aa3c1156b46834f59e70eab9b03914885bf8662e
Jabez Villan C. Pastores <152822367+Yaheezu@users.noreply.github.com>
2024-02-12 02:33:14 +0000

    Commit this message

--- Student.java
+++ Student.java
@@ -1,5 +1,25 @@
 public class Student {
     private String studentNumber;
+   
     private String lastName;
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+    public void setStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
 }
